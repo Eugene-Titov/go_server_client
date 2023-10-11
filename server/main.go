560822bb43b1ded685f5d2470e8e55cd9b67c38d
Eugene <etitov1980@mail.ru>
2023-10-11 16:50:17 +0300

    server listen

--- server/main.go
+++ server/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
+func serveConnection(conn net.Conn) {
+	fmt.Println("new connection")
+}
+
 func main() {
+	ln := createServer("tcp", "localhost:8100")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("error connection")
+			continue
+		}
+		serveConnection(conn)
+	}
 	fmt.Println("exit")
+}
+
+func createServer(protocol, port string) net.Listener {
+	ln, err := net.Listen(protocol, port)
+	if err != nil || ln == nil {
+		fmt.Println("error to create server")
+		os.Exit(1)
+	}
+	return ln
 }
